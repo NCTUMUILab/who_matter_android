9189d3369becbeec0f5e4f71958760985bdcdf25
changxing <changxing@baidu.com>
2016-03-04 18:14:59 +0800

    notification listener.

---
+++ app/src/androidTest/java/com/canking/notifymrg/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.canking.notifymrg;
+
+import android.util.Log;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by changxing on 16-3-3.
+ */
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+        Log.e("changxing","I am here");
+    }
+}
